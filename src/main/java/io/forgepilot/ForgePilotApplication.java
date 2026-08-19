9ac58aaf8c71b1ec05241d43613c977bc340dc99
stejas7 <stejas7@gmail.com>
2026-08-19 22:08:07 +0530

    feat: add ForgePilot application entrypoint

---
+++ src/main/java/io/forgepilot/ForgePilotApplication.java
@@ -0,0 +1,11 @@
+package io.forgepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgePilotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForgePilotApplication.class, args);
+    }
+}
