762f07d5ab99afc089f683b8a91ae4432beb516d
stejas7 <stejas7@gmail.com>
2026-08-21 15:47:21 +0530

    feat(p6): load backend panel styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './editor.css'
 import './p2.css'
 import './p5.css'
+import './p6.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
