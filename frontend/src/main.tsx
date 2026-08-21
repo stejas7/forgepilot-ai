d04657920af03065e1736e319306f1a5e461a420
stejas7 <stejas7@gmail.com>
2026-08-21 15:49:22 +0530

    feat(p7): load GitHub sync styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import './editor.css'
 import './p2.css'
 import './p5.css'
 import './p6.css'
+import './p7.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
