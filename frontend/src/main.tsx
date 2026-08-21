35aa0c1918d0468dbf7656cdb149d37f8fc21d21
stejas7 <stejas7@gmail.com>
2026-08-21 12:32:04 +0530

    style(p2): load plan and attachment styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './editor.css'
+import './p2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
